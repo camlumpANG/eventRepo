9a440fbcce088475bf85e56dc3aa10ad7ac9fb1b
Cameron Lump <cameronlumpMacAir@Camerons-MacBook-Air.local>
2022-10-31 18:20:19 -0400

    finished profile editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './models/user';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'eventApp';
+  newName:string = "";
+  // names:string[] = [
+  //   "Fred", "Julie", "Sam", "Olivia", "Adam", "Jennifer"
+  // ];
+  
+  user:User = {
+    name:"cameron",
+    age:26,
+    favoriteColor:"blue"
+    
+  }
+  displayEdit:boolean = false;
+
+  // changeTitle(newTitle:string):void {
+  //   this.title = newTitle
+  // }
+
+  // addName(){
+  //   this.names.push(this.newName);
+  // }
+
+  toggleEdit() {
+    this.displayEdit = !this.displayEdit;
+  }
 }
+
+
